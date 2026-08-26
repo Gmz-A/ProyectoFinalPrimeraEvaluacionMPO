4a6d285d0339493cdfb20463fcc5e96058f1c905
Gmz-A <aitorg21@gmail.com>
2026-08-26 13:27:22 +0200

    Primeras lineas

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("Hola mundo ")
+import os
+
+opcion_menu = 0
+
+while opcion_menu != 3:
+
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    print ("""
+    MENÚ
+        1. Empezar Cuestionario
+        2. Ranking
+        3. Salir
+    """)
+    opcion_menu = int(input("->"))
+
+    match opcion_menu:
+        case 1: 
+            print("preguntas")
+            pass
+        case 2: 
+            print("ranking")
+            pass
+        case 3:
+            print("Hasta lueo, muchas gracias :)")
+            pass
